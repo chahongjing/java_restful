211cdd6b82df8894cf3cfcb9de36f10abca0b344
chahongjing <310510906@qq.com>
2017-02-05 21:00:52 +0800

    restful服务优化

--- src/main/java/com/zjy/api/MyResources.java
+++ src/main/java/com/zjy/api/MyResources.java
@@ -1,6 +1,7 @@
 package com.zjy.api;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.zjy.baseframework.PropertiesHelper;
 import com.zjy.entities.UserInfo;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -22,7 +23,8 @@ public class MyResources {
 
     public static void main(String[] args) throws InterruptedException {
         ResourceConfig rc = new ResourceConfig().packages(MyResources.class.getPackage().getName()).register(JacksonJsonProvider.class);
-        GrizzlyHttpServerFactory.createHttpServer(URI.create("http://localhost:8080/api/rest"), rc);
+        String path = PropertiesHelper.getInstance().getProperties("serviceUrl");
+        GrizzlyHttpServerFactory.createHttpServer(URI.create(path), rc);
         while(true) {
             System.out.println(new Date());
             Thread.sleep(1000);
@@ -58,7 +60,8 @@ public class MyResources {
     public void test() {
         //Client client = ClientBuilder.newClient();
         Client client = ClientBuilder.newClient().register(JacksonJsonProvider.class);// 注册json 支持
-        WebTarget target = client.target("http://localhost:8080/api/rest/hello/users006");
+        String path = PropertiesHelper.getInstance().getProperties("serviceUrl");
+        WebTarget target = client.target(path + "/hello/users006");
         Response response = target.request().get();
         UserInfo user = response.readEntity(UserInfo.class);
         System.out.println(user.getUserName());
